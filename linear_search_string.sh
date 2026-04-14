91efd7a870abf4618b6965ae8221847c1fe8feb7
Tessa Tomy <tessatomy757@gmail.com>
2026-04-14 13:07:44 +0530

    add linear search for string array input in bash

---
+++ linear_search_string.sh
@@ -0,0 +1,18 @@
+echo "Array :"
+read -a a
+n=${#a[@]}
+x=1
+
+read -p "Search Key : " s
+
+for i in ${a[@]}
+do
+    if [[ $i == "$s" ]]
+    then
+        echo "Element found at pos : $x"
+        exit 1
+    fi
+    let x++
+done
+
+echo "Element not found !!"
